4aef9ea786dc75372ccdc9d8d0efa3fca637b0a1
Alvaro <alvarof260@gmail.com>
2023-08-21 21:33:55 -0300

    refactor: changes from memory to fs

---
+++ src/product-manager.js
@@ -0,0 +1,109 @@
+import { readFile, writeFile } from 'node:fs/promises'
+import { existsSync } from 'node:fs'
+
+export class ProductManager {
+  #path
+  constructor (path) {
+    this.#path = path
+    this.#init()
+  }
+
+  async #init () {
+    if (!existsSync(this.#path)) {
+      await writeFile(this.#path, JSON.stringify([], null, '/t'))
+    }
+  }
+
+  async getProducts () {
+    const response = await readFile(this.#path, 'utf-8')
+    const parseResponse = JSON.parse(response)
+    return parseResponse
+  }
+
+  async addProduct (product) {
+    const response = await this.getProducts()
+    const found = response.find(el => el.code === product.code)
+    if (found) return
+    const id = this.generatorID(response)
+    response.push({ id, ...product })
+    await writeFile(this.#path, JSON.stringify(response, null, '\t'))
+    return { id, ...product }
+  }
+
+  generatorID (products) {
+    if (products.length === 0) return 1
+    return products[products.length - 1].id + 1
+  }
+
+  async getProductByID (idProd) {
+    const products = await this.getProducts()
+    const product = products.find((el) => el.id === parseInt(idProd))
+    if (product) {
+      return product
+    } else {
+      throw new Error(`id:${idProd}, not found`)
+    }
+  }
+
+  async productUpdate (itemId, updatedProduct) {
+    const products = await this.getProducts()
+    const productsUpdates = products.map(item => {
+      if (item.id === parseInt(itemId)) {
+        return { ...item, ...updatedProduct }
+      } else {
+        return item
+      }
+    })
+    await writeFile(this.#path, JSON.stringify(productsUpdates, null, '\t'))
+    return productsUpdates.find(el => el.id === parseInt(itemId))
+  }
+
+  async productDelete (itemId) {
+    const products = await this.getProducts()
+    const found = products.find(el => el.id === parseInt(itemId))
+    if (!found) return
+    const position = products.findIndex(el => el.id === parseInt(itemId))
+    const productsUpdates = products.splice(position, 1)
+    await writeFile(this.#path, JSON.stringify(products, null, '\t'))
+    return productsUpdates
+  }
+}
+
+/* const productManager = new ProductManager('products.json')
+
+const test = async () => {
+  console.log('---------------!----------------')
+  console.log(await productManager.getProducts())
+
+  console.log('---------------!----------------')
+  console.log(await productManager.addProduct({ title: 'sin titulo', description: 'sin desc.', price: 250, thumbnail: 'no imagen', code: 'b0452', stock: 10 }))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.getProducts())
+
+  console.log('---------------!----------------')
+  console.log(await productManager.addProduct({ title: 'sin titulo', description: 'sin desc.', price: 250, thumbnail: 'no imagen', code: 'b0452', stock: 10 }))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.getProducts())
+
+  console.log('---------------!----------------')
+  console.log(await productManager.addProduct({ title: 'sin titulo', description: 'sin desc.', price: 35000, thumbnail: 'no imagen', code: 'b5452', stock: 15 }))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.getProducts())
+
+  console.log('---------------!----------------')
+  console.log(await productManager.getProductByID(1))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.productUpdate(1, { title: 'titulo cappoooo!' }))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.productDelete(1))
+
+  console.log('---------------!----------------')
+  console.log(await productManager.getProducts())
+}
+
+test() */
--- src/routers/products-router.js
+++ src/routers/products-router.js
@@ -1,208 +1,49 @@
 import { Router } from 'express'
 import { validatePartialProduct, validateProduct } from '../schema/product.js'
-
-const products = [
-  {
-    id: 1,
-    title: 'Smartphone',
-    description: 'High-end smartphone with advanced features.',
-    code: 'PHN001',
-    price: 799.99,
-    status: true,
-    stock: 100,
-    category: 'Electronics',
-    thumbnails: [
-      'smartphone_thumbnail1.jpg',
-      'smartphone_thumbnail2.jpg',
-      'smartphone_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 2,
-    title: 'Running Shoes',
-    description: 'Comfortable running shoes for your active lifestyle.',
-    code: 'SHS002',
-    price: 89.99,
-    status: true,
-    stock: 50,
-    category: 'Clothing',
-    thumbnails: [
-      'shoes_thumbnail1.jpg',
-      'shoes_thumbnail2.jpg',
-      'shoes_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 3,
-    title: 'Cotton Sheets',
-    description: "Soft and breathable cotton sheets for a good night's sleep.",
-    code: 'HME003',
-    price: 39.99,
-    status: true,
-    stock: 200,
-    category: 'Home',
-    thumbnails: [
-      'sheets_thumbnail1.jpg',
-      'sheets_thumbnail2.jpg',
-      'sheets_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 4,
-    title: 'Organic Snacks Box',
-    description: 'A box of assorted organic and healthy snacks.',
-    code: 'GRB004',
-    price: 24.99,
-    status: true,
-    stock: 30,
-    category: 'Groceries',
-    thumbnails: [
-      'snacks_thumbnail1.jpg',
-      'snacks_thumbnail2.jpg',
-      'snacks_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 5,
-    title: 'Bluetooth Speaker',
-    description: 'Portable Bluetooth speaker for music on the go.',
-    code: 'SPK005',
-    price: 49.99,
-    status: true,
-    stock: 75,
-    category: 'Electronics',
-    thumbnails: [
-      'speaker_thumbnail1.jpg',
-      'speaker_thumbnail2.jpg',
-      'speaker_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 6,
-    title: 'Graphic T-Shirt',
-    description: 'Stylish graphic t-shirt with unique design.',
-    code: 'CLT006',
-    price: 19.99,
-    status: true,
-    stock: 40,
-    category: 'Clothing',
-    thumbnails: [
-      'shirt_thumbnail1.jpg',
-      'shirt_thumbnail2.jpg',
-      'shirt_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 7,
-    title: 'Cookware Set',
-    description: 'Complete set of non-stick cookware for your kitchen.',
-    code: 'KTC007',
-    price: 149.99,
-    status: true,
-    stock: 20,
-    category: 'Home',
-    thumbnails: [
-      'cookware_thumbnail1.jpg',
-      'cookware_thumbnail2.jpg',
-      'cookware_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 8,
-    title: 'Fresh Produce Bundle',
-    description: 'Assortment of fresh and locally sourced produce.',
-    code: 'GRP008',
-    price: 29.99,
-    status: true,
-    stock: 15,
-    category: 'Groceries',
-    thumbnails: [
-      'produce_thumbnail1.jpg',
-      'produce_thumbnail2.jpg',
-      'produce_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 9,
-    title: 'Wireless Earbuds',
-    description: 'Wireless earbuds with noise-cancellation technology.',
-    code: 'EAR009',
-    price: 79.99,
-    status: true,
-    stock: 60,
-    category: 'Electronics',
-    thumbnails: [
-      'earbuds_thumbnail1.jpg',
-      'earbuds_thumbnail2.jpg',
-      'earbuds_thumbnail3.jpg'
-    ]
-  },
-  {
-    id: 10,
-    title: 'Denim Jeans',
-    description: 'Classic denim jeans for a casual yet stylish look.',
-    code: 'CLT010',
-    price: 59.99,
-    status: true,
-    stock: 25,
-    category: 'Clothing',
-    thumbnails: [
-      'jeans_thumbnail1.jpg',
-      'jeans_thumbnail2.jpg',
-      'jeans_thumbnail3.jpg'
-    ]
-  }
-]
+import { ProductManager } from '../product-manager.js'
 
 const router = Router()
+const DM = new ProductManager('./src/data/products.json')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const products = await DM.getProducts()
   const { limit } = req.query
   if (!limit) return res.status(200).json(products)
   const newProducts = products.slice(0, limit)
   res.status(200).json(newProducts)
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const id = req.params.id
-  const product = products.find(item => item.id === parseInt(id))
+  const product = await DM.getProductByID(id)
   if (!product) return res.status(404).json({ error: 'Product not found' })
   res.status(200).json(product)
 })
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const product = validateProduct(req.body)
   if (product.error) return res.status(400).json({ error: product.error })
-  const id = products.length === 0 ? 1 : products[products.length - 1].id + 1
   const newProduct = {
-    id,
     ...product.data
   }
-  products.push(newProduct)
-  res.status(201).json(newProduct)
+  const addProduct = await DM.addProduct(newProduct)
+  res.status(201).json(addProduct)
 })
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const result = validatePartialProduct(req.body)
   if (result.error) return res.status(400).json({ error: result.error })
 
   const id = req.params.id
-  const indexProduct = products.findIndex(item => item.id === parseInt(id))
-  if (indexProduct < 0) return res.status(400).json({ error: 'Product not found' })
-  const olderProduct = products[indexProduct]
-  products[indexProduct] = {
-    ...olderProduct,
-    ...result.data
-  }
-  res.status(200).json(products[indexProduct])
+  const productUpdate = await DM.productUpdate(id, { ...result.data })
+  res.status(200).json(productUpdate)
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const id = req.params.id
-  const indexProduct = products.findIndex(item => item.id === parseInt(id))
-  if (indexProduct < 0) return res.status(404).json({ error: 'Product not found' })
-  const productDeleted = products[indexProduct]
-  products.splice(indexProduct, 1)
+  const productDeleted = await DM.productDelete(id)
+  if (!productDeleted) return res.status(404).json({ error: 'Product not found' })
   res.status(201).json(productDeleted)
 })
+
 export default router
